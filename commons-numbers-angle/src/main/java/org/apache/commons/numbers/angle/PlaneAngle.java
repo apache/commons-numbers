1aa1ddcd5a3bfdb99ae38bc5c761a80adb166ad6
Gilles Sadowski <gilles@harfang.homelinux.org>
2017-05-12 17:05:42 +0200

    NUMBERS-37: New "PlaneAngle" class.
    New module for angle-related utilities.

---
+++ commons-numbers-angle/src/main/java/org/apache/commons/numbers/angle/PlaneAngle.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.numbers.angle;
+
+/**
+ * Represents the <a href="https://en.wikipedia.org/wiki/Angle">angle</a> concept.
+ */
+public class PlaneAngle {
+    /** Conversion factor. */
+    private static final double HALF_TURN = 0.5;
+    /** Conversion factor. */
+    private static final double TO_RADIANS = 2 * Math.PI;
+    /** Conversion factor. */
+    private static final double FROM_RADIANS = 1d / TO_RADIANS;
+    /** Conversion factor. */
+    private static final double TO_DEGREES = 360;
+    /** Conversion factor. */
+    private static final double FROM_DEGREES = 1d / TO_DEGREES;
+    /** Zero. */
+    private static final PlaneAngle ZERO = new PlaneAngle(0);
+    /** Value (in turns). */
+    private final double value;
+
+    /**
+     * @param value Value in turns.
+     */
+    private PlaneAngle(double value) {
+        this.value = value;
+    }
+
+    /**
+     * @param angle (in <a href="https://en.wikipedia.org/wiki/Turn_%28geometry%29">turns</a>).
+     * @return a new intance.
+     */
+    public static PlaneAngle ofTurns(double angle) {
+        return new PlaneAngle(angle);
+    }
+    /**
+     * @param angle (in <a href="https://en.wikipedia.org/wiki/Radian">radians</a>).
+     * @return a new intance.
+     */
+    public static PlaneAngle ofRadians(double angle) {
+        return new PlaneAngle(angle * FROM_RADIANS);
+    }
+    /**
+     * @param angle (in <a href="https://en.wikipedia.org/wiki/Degree_%28angle%29">degrees</a>).
+     * @return a new intance.
+     */
+    public static PlaneAngle ofDegrees(double angle) {
+        return new PlaneAngle(angle * FROM_DEGREES);
+    }
+
+    /**
+     * @return the value in <a href="https://en.wikipedia.org/wiki/Turn_%28geometry%29">turns</a>.
+     */
+    public double toTurns() {
+        return value;
+    }
+    /**
+     * @return the value in <a href="https://en.wikipedia.org/wiki/Radian">radians</a>.
+     */
+    public double toRadians() {
+        return value * TO_RADIANS;
+    }
+    /**
+     * @return the value in <a href="https://en.wikipedia.org/wiki/Degree_%28angle%29">degrees</a>.
+     */
+    public double toDegrees() {
+        return value * TO_DEGREES;
+    }
+
+    /**
+     * Normalize an angle in an interval of size 1 turn around a
+     * center value.
+     *
+     * @param center Center of the desired interval for the result.
+     * @return {@code a - 2 * k} with integer {@code k} such that
+     * {@code center - 0.5 <= a - 2 * k <= center + 0.5} (in turns).
+     */
+    public PlaneAngle normalize(PlaneAngle center) {
+        return new PlaneAngle(value - Math.floor(value + HALF_TURN - center.value));
+    }
+
+    /**
+     * Normalize within the interval centered at 0.
+     *
+     * @return {@code a - 2 * k} with integer {@code k} such that
+     * {@code -0.5 <= a - 2 * k <= 0.5} (in turns).
+     */
+    public PlaneAngle normalize() {
+        return normalize(ZERO);
+    }
+
+    /**
+     * Test for equality with another object.
+     * Objects are considered to be equal if the two values are exactly the
+     * same, or both are {@code Double.NaN}.
+     *
+     * @param other Object to test for equality with this instance.
+     * @return {@code true} if the objects are equal, {@code false} if
+     * {@code other} is {@code null}, not an instance of {@code PlaneAngle},
+     * or not equal to this instance.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other instanceof PlaneAngle){
+            return new Double(value).equals(new Double(((PlaneAngle) other).value));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return new Double(value).hashCode();
+    }
+}
---
+++ commons-numbers-angle/src/test/java/org/apache/commons/numbers/angle/PlaneAngleTest.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.apache.commons.numbers.angle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test cases for the {@link PlaneAngle} class.
+ */
+public class PlaneAngleTest {
+    @Test
+    public void testConversionTurns() {
+        final double value = 12.3456;
+        final PlaneAngle a = PlaneAngle.ofTurns(value);
+        Assert.assertEquals(value, a.toTurns(), Math.ulp(value));
+    }
+
+    @Test
+    public void testConversionRadians() {
+        final double value = 12.3456;
+        final PlaneAngle a = PlaneAngle.ofRadians(value);
+        Assert.assertEquals(value, a.toRadians(), Math.ulp(value));
+    }
+
+    @Test
+    public void testConversionDegrees() {
+        final double value = 12.3456;
+        final PlaneAngle a = PlaneAngle.ofDegrees(value);
+        Assert.assertEquals(value, a.toDegrees(), Math.ulp(value));
+    }
+
+    @Test
+    public void testNormalizeRadians() {
+        for (double a = -15.0; a <= 15.0; a += 0.1) {
+            for (double b = -15.0; b <= 15.0; b += 0.2) {
+                final PlaneAngle aA = PlaneAngle.ofRadians(a);
+                final PlaneAngle aB = PlaneAngle.ofRadians(b);
+                final double c = aA.normalize(aB).toRadians();
+                Assert.assertTrue((b - Math.PI) <= c);
+                Assert.assertTrue(c <= (b + Math.PI));
+                double twoK = Math.rint((a - c) / Math.PI);
+                Assert.assertEquals(c, a - twoK * Math.PI, 1e-14);
+            }
+        }
+    }
+
+    @Test
+    public void testNormalizeMixed() {
+        for (double a = -15.0; a <= 15.0; a += 0.1) {
+            for (double b = -15.0; b <= 15.0; b += 0.2) {
+                final PlaneAngle aA = PlaneAngle.ofDegrees(a);
+                final PlaneAngle aB = PlaneAngle.ofRadians(b);
+                final double c = aA.normalize(aB).toTurns();
+                Assert.assertTrue((aB.toTurns() - 0.5) <= c);
+                Assert.assertTrue(c <= (aB.toTurns() + 0.5));
+                double twoK = Math.rint((aA.toTurns() - c));
+                Assert.assertEquals(c, aA.toTurns() - twoK, 1e-14);
+            }
+        }
+    }
+
+    @Test
+    public void testNormalizeSimple1() {
+        final double value = 1.25;
+        final double expected = 0.25;
+        final double actual = PlaneAngle.ofTurns(value).normalize().toTurns();
+        final double tol = Math.ulp(expected);
+        Assert.assertEquals(expected, actual, tol);
+    }
+    @Test
+    public void testNormalizeSimple2() {
+        final double value = 0.75;
+        final double expected = -0.25;
+        final double actual = PlaneAngle.ofTurns(value).normalize().toTurns();
+        final double tol = Math.ulp(expected);
+        Assert.assertEquals(expected, actual, tol);
+    }
+    @Test
+    public void testNormalizeSimple3() {
+        final double value = 0.5 + 1e-10;
+        final double expected = -0.5 + 1e-10;
+        final double actual = PlaneAngle.ofTurns(value).normalize().toTurns();
+        final double tol = Math.ulp(expected);
+        Assert.assertEquals(expected, actual, tol);
+    }
+    @Test
+    public void testNormalizeSimple4() {
+        final double value = 5 * Math.PI / 4;
+        final double expected = Math.PI * (1d / 4 - 1);
+        final double actual = PlaneAngle.ofRadians(value).normalize().toRadians();
+        final double tol = Math.ulp(expected);
+        Assert.assertEquals(expected, actual, tol);
+    }
+
+    @Test
+    public void testHashCode() {
+        // Test assumes that the internal representation is in "turns".
+        final double value = -123.456789;
+        final int expected = new Double(value).hashCode();
+        final int actual = PlaneAngle.ofTurns(value).hashCode();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testEquals1() {
+        final double value = 12345.6789;
+        final PlaneAngle a = PlaneAngle.ofRadians(value);
+        final PlaneAngle b = PlaneAngle.ofRadians(value);
+        Assert.assertTrue(a.equals(b));
+    }
+    @Test
+    public void testEquals2() {
+        final PlaneAngle a = PlaneAngle.ofRadians(153768.373486587);
+        final PlaneAngle b = null;
+        Assert.assertFalse(a.equals(b));
+    }
+    @Test
+    public void testEquals3() {
+        final double value = 0.987654321;
+        final PlaneAngle a = PlaneAngle.ofRadians(value);
+        final PlaneAngle b = PlaneAngle.ofRadians(value + 1e-16);
+        Assert.assertFalse(a.equals(b));
+    }
+}
