de85ffc2843e43e0c0e426151a995d0c7dc77673
Gilles Sadowski <gilles@harfang.homelinux.org>
2018-01-30 16:49:41 +0100

    NUMBERS-51: Class "FP64" ("double" with "Field" API).

---
+++ commons-numbers-field/src/main/java/org/apache/commons/numbers/field/FP64Field.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.numbers.field;
+
+/**
+ * {@link Double} field.
+ */
+public class FP64Field extends AbstractField<FP64> {
+    /** 0d */
+    private static final FP64 ZERO = new FP64(Double.valueOf(0));
+    /** 1d */
+    private static final FP64 ONE = new FP64(Double.valueOf(1));
+
+    /** {@inheritDoc} */
+    @Override
+    public FP64 one() {
+        return ONE;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public FP64 zero() {
+        return ZERO;
+    }
+}
