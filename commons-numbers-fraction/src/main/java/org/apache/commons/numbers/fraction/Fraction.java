7672acea193df8dec70cd70b745557c3591c0caf
Eric Barnhill <ebarnhill@irhythmtech.com>
2018-10-29 12:10:36 -0700

    NUMBERS-75: Removal of private constructors and replacement with of* factory methods

--- commons-numbers-fraction/src/main/java/org/apache/commons/numbers/fraction/Fraction.java
+++ commons-numbers-fraction/src/main/java/org/apache/commons/numbers/fraction/Fraction.java
@@ -19,16 +19,15 @@ package org.apache.commons.numbers.fraction;
 import java.io.Serializable;
 
 import org.apache.commons.numbers.core.ArithmeticUtils;
-import org.apache.commons.numbers.core.NativeOperators;
 
 /**
  * Representation of a rational number.
+ *
+ * implements Serializable since 2.0
  */
 public class Fraction
     extends Number
-    implements Comparable<Fraction>,
-               Serializable,
-               NativeOperators<Fraction> {
+    implements Comparable<Fraction>, Serializable {
 
     /** Serializable version identifier */
     private static final long serialVersionUID = 3698073679419233275L;
@@ -373,18 +372,6 @@ public class Fraction
         return (long)doubleValue();
     }
 
-    /** {@inheritDoc} */
-    @Override
-    public Fraction zero() {
-        return ZERO;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public Fraction one() {
-        return ONE;
-    }
-
     /**
      * Return the additive inverse of this fraction.
      * @return the negation of this fraction.
@@ -523,25 +510,6 @@ public class Fraction
      */
     public Fraction multiply(final int i) {
         return multiply(of(i));
-    }
-
-    /**
-     * @param n Power.
-     * @return {@code this^n}
-     */
-    public Fraction pow(final int n) {
-        if (n == 0) {
-            return ONE;
-        }
-        if (numerator == 0) {
-            return this;
-        }
-
-        return n < 0 ?
-            new Fraction(ArithmeticUtils.pow(denominator, -n),
-                         ArithmeticUtils.pow(numerator, -n)) :
-            new Fraction(ArithmeticUtils.pow(numerator, n),
-                         ArithmeticUtils.pow(denominator, n));
     }
 
     /**
