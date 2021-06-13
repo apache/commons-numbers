b34fa4cf4713bd0bd765dcb3fe633abdf8ff4e84
Gilles Sadowski <gilleseran@gmail.com>
2021-06-13 14:37:58 +0200

    NUMBERS-164: Sort utility (ported with API changes from Commons Math).

---
+++ commons-numbers-arrays/src/main/java/org/apache/commons/numbers/arrays/SortInPlace.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.numbers.arrays;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+
+/**
+ * Sort an array and perform the same reordering of entries on other arrays.
+ * For example, if
+ * <ul>
+ *  <li>{@code x = [3, 1, 2]}</li>
+ *  <li>{@code y = [1, 2, 3]}</li>
+ *  <li>{@code z = [0, 5, 7]}</li>
+ * </ul>
+ * then {@code Sort.ASCENDING.accept(x, y, z)} will update those arrays:
+ * <ul>
+ *  <li>{@code x = [1, 2, 3]}</li>
+ *  <li>{@code y = [2, 3, 1]}</li>
+ *  <li>{@code z = [5, 7, 0]}</li>
+ * </ul>
+ */
+public enum SortInPlace implements BiConsumer<double[], double[][]> {
+    /** Sort in ascending order. */
+    ASCENDING((o1, o2) -> Double.compare(o1.key(), o2.key())),
+    /** Sort in descending order. */
+    DESCENDING(ASCENDING.comparator.reversed());
+
+    /** Comparator. */
+    private final Comparator<PairDoubleInteger> comparator;
+
+    /**
+     * @param comparator Comparator.
+     */
+    SortInPlace(Comparator<PairDoubleInteger> comparator) {
+        this.comparator = comparator;
+    }
+
+    /**
+     * Sorts in place.
+     *
+     * @param x Array to be sorted and used as a pattern for permutation of
+     * the other arrays.
+     * @param yList Set of arrays whose permutations of entries will follow
+     * those performed on {@code x}.
+     * @throws IllegalArgumentException if not all arrays have the same size.
+     */
+    @Override
+    public void accept(double[] x,
+                       double[]... yList) {
+        final int yListLen = yList.length;
+        final int len = x.length;
+
+        for (int j = 0; j < yListLen; j++) {
+            final double[] y = yList[j];
+            if (y.length != len) {
+                throw new IllegalArgumentException("Size mismatch: " +
+                                                   y.length + " != " + len);
+            }
+        }
+
+        // Associate each abscissa "x[i]" with its index "i".
+        final List<PairDoubleInteger> list = new ArrayList<>(len);
+        for (int i = 0; i < len; i++) {
+            list.add(new PairDoubleInteger(x[i], i));
+        }
+
+        // Sort.
+        Collections.sort(list, comparator);
+
+        // Modify the original array so that its elements are in the prescribed order.
+        // Retrieve indices of original locations.
+        final int[] indices = new int[len];
+        for (int i = 0; i < len; i++) {
+            final PairDoubleInteger e = list.get(i);
+            x[i] = e.key();
+            indices[i] = e.value();
+        }
+
+        // In every associated array, move the elements to their new location.
+        for (int j = 0; j < yListLen; j++) {
+            // Input array will be modified in place.
+            final double[] yInPlace = yList[j];
+            final double[] yOrig = Arrays.copyOf(yInPlace, len);
+
+            for (int i = 0; i < len; i++) {
+                yInPlace[i] = yOrig[indices[i]];
+            }
+        }
+    }
+
+    /**
+     * Helper data structure holding a (double, integer) pair.
+     */
+    private static class PairDoubleInteger {
+        /** Key. */
+        private final double key;
+        /** Value. */
+        private final int value;
+
+        /**
+         * @param key Key.
+         * @param value Value.
+         */
+        PairDoubleInteger(double key,
+                          int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        /** @return the key. */
+        double key() {
+            return key;
+        }
+
+        /** @return the value. */
+        int value() {
+            return value;
+        }
+    }
+}
---
+++ commons-numbers-arrays/src/test/java/org/apache/commons/numbers/arrays/SortInPlaceTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.numbers.arrays;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test cases for the {@link SortInPlace} class.
+ *
+ */
+class SortInPlaceTest {
+    @Test
+    public void testAscending() {
+        final double[] x = {2, 5, -3, 1,  4};
+        final double[] y = {4, 25, 9, 1, 16};
+        final double[] z = {8, -125, 27, 1, 64};
+
+        SortInPlace.ASCENDING.accept(x, y, z);
+
+        final double[] xE = {-3, 1, 2, 4, 5};
+        final double[] yE = {9, 1, 4, 16, 25};
+        final double[] zE = {27, 1, 8, 64, -125};
+
+        Assertions.assertTrue(Arrays.equals(xE, x));
+        Assertions.assertTrue(Arrays.equals(yE, y));
+        Assertions.assertTrue(Arrays.equals(zE, z));
+    }
+
+    @Test
+    public void testDescending() {
+        final double[] x = {2, 5, -3, 1, 4};
+        final double[] y = {4, 25, 9, 1, 16};
+        final double[] z = {8, -125, 27, 1, 64};
+
+        SortInPlace.DESCENDING.accept(x, y, z);
+
+        final double[] xE = {5, 4, 2, 1, -3};
+        final double[] yE = {25, 16, 4, 1, 9};
+        final double[] zE = {-125, 64, 8, 1, 27};
+
+        Assertions.assertTrue(Arrays.equals(xE, x));
+        Assertions.assertTrue(Arrays.equals(yE, y));
+        Assertions.assertTrue(Arrays.equals(zE, z));
+    }
+
+    // Example in Javadoc.
+    @Test
+    public void testJavadocExample() {
+        final double[] x = {3, 1, 2};
+        final double[] y = {1, 2, 3};
+        final double[] z = {0, 5, 7};
+
+        SortInPlace.ASCENDING.accept(x, y, z);
+
+        final double[] xE = {1, 2, 3};
+        final double[] yE = {2, 3, 1};
+        final double[] zE = {5, 7, 0};
+
+        Assertions.assertTrue(Arrays.equals(xE, x));
+        Assertions.assertTrue(Arrays.equals(yE, y));
+        Assertions.assertTrue(Arrays.equals(zE, z));
+    }
+
+    @Test
+    public void testPreconditions() {
+        final double[] nullArray = null;
+        final double[] one = {1};
+        final double[] two = {1, 2};
+        final double[] onep = {2};
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> SortInPlace.ASCENDING.accept(one, two));
+        Assertions.assertThrows(NullPointerException.class, () -> SortInPlace.ASCENDING.accept(one, nullArray));
+        Assertions.assertThrows(NullPointerException.class, () -> SortInPlace.ASCENDING.accept(one, onep, nullArray));
+    }
+}
