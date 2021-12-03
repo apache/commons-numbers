274818ccab651e51a47985a1ea54ef069b524adb
aherbert <aherbert@apache.org>
2021-12-03 13:47:57 +0000

    NUMBERS-178: Tabulate all representable factorials in FactorialDouble
    Values are verified using BigInteger computation of n!
    
    This change deprecates the now redundant caching functionality.

--- commons-numbers-combinatorics/src/test/java/org/apache/commons/numbers/combinatorics/FactorialDoubleTest.java
+++ commons-numbers-combinatorics/src/test/java/org/apache/commons/numbers/combinatorics/FactorialDoubleTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.commons.numbers.combinatorics;
 
+import java.math.BigInteger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -23,46 +24,12 @@ import org.junit.jupiter.api.Test;
  * Test cases for the {@link FactorialDouble} class.
  */
 class FactorialDoubleTest {
+    /** The largest representable factorial (n=170). */
+    private static final int MAX_N = 170;
+
     @Test
     void testFactorialZero() {
         Assertions.assertEquals(1, FactorialDouble.create().value(0), "0!");
-    }
-
-    @Test
-    void testFactorialDirect() {
-        for (int i = 1; i < 21; i++) {
-            Assertions.assertEquals(
-                    factorialDirect(i), FactorialDouble.create().value(i), i + "!");
-        }
-    }
-
-    @Test
-    void testLargestFactorialDouble() {
-        final int n = 170;
-        Assertions.assertNotEquals(
-            Double.POSITIVE_INFINITY, FactorialDouble.create().value(n), () -> n + "!");
-    }
-
-    @Test
-    void testFactorialDoubleTooLarge() {
-        final int n = 171;
-        // Verify the limit. Start at the largest representable factorial as a long: 20!
-        double f = 2432902008176640000L;
-        for (int i = 21; i < n; i++) {
-            f *= i;
-        }
-        Assertions.assertTrue(Double.isFinite(f), "170! is finite");
-        f *= n;
-        Assertions.assertFalse(Double.isFinite(f), "171! is infinite");
-        Assertions.assertEquals(
-                Double.POSITIVE_INFINITY, FactorialDouble.create().value(n), () -> n + "!");
-    }
-
-    @Test
-    void testNonPositiveArgumentWithCache() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-            () -> FactorialDouble.create().withCache(-1)
-        );
     }
 
     @Test
@@ -73,76 +40,37 @@ class FactorialDoubleTest {
     }
 
     @Test
-    void testCompareDirectWithoutCache() {
-        // This test shows that delegating to the "Gamma" class will also lead to a
-        // less accurate result.
-
-        final int max = 100;
+    void testFactorials() {
         final FactorialDouble f = FactorialDouble.create();
-
-        for (int i = 0; i < max; i++) {
-            final double expected = factorialDirect(i);
-            Assertions.assertEquals(
-                    expected, f.value(i), 100 * Math.ulp(expected), i + "! ");
+        // Start at 0!
+        BigInteger value = BigInteger.ONE;
+        for (int n = 1; n <= MAX_N; n++) {
+            // n! = (n-1)! * n
+            value = value.multiply(BigInteger.valueOf(n));
+            Assertions.assertEquals(value.doubleValue(), f.value(n));
         }
     }
 
     @Test
-    void testCompareDirectWithCache() {
-        final int max = 100;
-        final FactorialDouble f = FactorialDouble.create().withCache(max);
-
-        for (int i = 0; i < max; i++) {
-            final double expected = factorialDirect(i);
-            Assertions.assertEquals(
-                    expected, f.value(i), 100 * Math.ulp(expected), i + "! ");
+    void testFactorialDoubleTooLarge() {
+        final FactorialDouble f = FactorialDouble.create();
+        // Long avoids overflow to negative
+        for (long n = MAX_N + 1; n < Integer.MAX_VALUE; n *= 2) {
+            Assertions.assertEquals(Double.POSITIVE_INFINITY, f.value((int) n));
         }
-    }
-
-    @Test
-    void testCacheIncrease() {
-        final int max = 100;
-        final FactorialDouble f1 = FactorialDouble.create().withCache(max);
-        final FactorialDouble f2 = f1.withCache(2 * max);
-
-        final int val = max + max / 2;
-        Assertions.assertEquals(f1.value(val), f2.value(val));
-    }
-
-    @Test
-    void testZeroCache() {
-        // Ensure that no exception is thrown.
-        final FactorialDouble f = FactorialDouble.create().withCache(0);
-        Assertions.assertEquals(1, f.value(0));
-        Assertions.assertEquals(1, f.value(1));
-    }
-
-    @Test
-    void testUselessCache() {
-        Assertions.assertDoesNotThrow(() -> {
-            LogFactorial.create().withCache(1);
-            LogFactorial.create().withCache(2);
-        });
-    }
-
-    @Test
-    void testCacheDecrease() {
-        final int max = 100;
-        final FactorialDouble f1 = FactorialDouble.create().withCache(max);
-        final FactorialDouble f2 = f1.withCache(max / 2);
-
-        final int val = max / 4;
-        Assertions.assertEquals(f1.value(val), f2.value(val));
     }
 
     /**
-     * Direct multiplication implementation.
+     * Test the {@link FactorialDouble#withCache(int)} method does nothing.
+     * This method was deprecated since 1.1.
+     * It now allows calling the method with an invalid cache size.
      */
-    private double factorialDirect(int n) {
-        double result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
+    @SuppressWarnings("deprecation")
+    @Test
+    void testWithCacheReturnsThis() {
+        final FactorialDouble f = FactorialDouble.create();
+        for (int cacheSize : new int[] {-1, 0, 10000000}) {
+            Assertions.assertSame(f, f.withCache(cacheSize));
         }
-        return result;
     }
 }
