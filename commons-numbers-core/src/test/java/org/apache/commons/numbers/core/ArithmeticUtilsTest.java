22fa3b519e725cdcd293a99945efa15339bc9d63
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-13 18:50:06 +0100

    [NUMB-196] implemented methods that check if a given number is between the other 2, but excluding the other 2

--- commons-numbers-core/src/test/java/org/apache/commons/numbers/core/ArithmeticUtilsTest.java
+++ commons-numbers-core/src/test/java/org/apache/commons/numbers/core/ArithmeticUtilsTest.java
@@ -660,113 +660,38 @@ class ArithmeticUtilsTest {
     }
 
     @Test
-    void testIsBetweenLeftExclusive() {
-        BigInteger minus1Int = BigInteger.valueOf(-1);
-        BigDecimal minus1Dec = BigDecimal.valueOf(-1);
-        BigInteger minus10000Int = BigInteger.valueOf(-10000);
-        BigInteger plus10000Int = BigInteger.valueOf(10000);
-        BigDecimal minus10000Dec = BigDecimal.valueOf(-10000);
-        BigDecimal plus10000Dec = BigDecimal.valueOf(10000);
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(292805461453366231L, -2147479015L, 292805461453366231L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(292805461453366231L, 292805461453366231L, 292805461453366231L));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(292805461453366231L, -2147479015L, 292805461453366232L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(292805461453366231L, -2147479015L, 292805461453366230L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-292805461453366231L, -2147479015L, 292805461453366230L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-0.0d, 0, 1));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-0.0d, 0.0d, 1));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-0.0d, 0.0d, 1.0d));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-0.0d, 0.0d, 1.0f));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-0.0d, 0.0f, 1.0f));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-0.0d, 0.0f, 1.0d));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(-0.0d, 0.0f, 1));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(0.0d, -0.0f, 1));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenLeftExclusive(minus1Int, BigInteger.ZERO, BigInteger.ONE));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(minus1Int, minus10000Int, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(minus1Dec, minus10000Int, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(minus1Dec, minus10000Dec, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(minus1Dec, minus10000Dec, plus10000Dec));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(minus1Int, minus10000Dec, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(minus1Int, minus10000Int, plus10000Dec));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenLeftExclusive(minus1Int, minus10000Dec, plus10000Dec));
+    void testIsBetweenExclusive_int() {
+        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461, -2147479015, 292805461));
+        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(292805461, -2147479015, 292805462));
+        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461, -2147479015, 292805460));
+        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(-292805461, -2147479015, 292805460));
+        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0, 0, 1));
+        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(0, -0, 1));
+        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(0, 0, 1));
+        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-1, 0, 1));
+        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(-1, -10000, 10000));
     }
 
     @Test
-    void testIsBetweenRightExclusive() {
-        BigInteger minus1Int = BigInteger.valueOf(-1);
-        BigDecimal minus1Dec = BigDecimal.valueOf(-1);
-        BigInteger minus10000Int = BigInteger.valueOf(-10000);
-        BigInteger plus10000Int = BigInteger.valueOf(10000);
-        BigDecimal minus10000Dec = BigDecimal.valueOf(-10000);
-        BigDecimal plus10000Dec = BigDecimal.valueOf(10000);
-        Assertions.assertFalse(ArithmeticUtils.isBetweenRightExclusive(292805461453366231L, -2147479015L, 292805461453366231L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenRightExclusive(292805461453366231L, 292805461453366231L, 292805461453366231L));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(292805461453366231L, -2147479015L, 292805461453366232L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenRightExclusive(292805461453366231L, -2147479015L, 292805461453366230L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenRightExclusive(-292805461453366231L, -2147479015L, 292805461453366230L));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(-0.0d, 0, 1));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(-0.0d, 0.0d, 1));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(-0.0d, 0.0d, 1.0d));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(-0.0d, 0.0d, 1.0f));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(-0.0d, 0.0f, 1.0f));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(-0.0d, 0.0f, 1.0d));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(-0.0d, 0.0f, 1));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(0.0d, -0.0f, 1));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenRightExclusive(BigInteger.ZERO, BigDecimal.ONE, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenRightExclusive(BigInteger.ZERO, BigInteger.ONE, BigDecimal.ONE));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenRightExclusive(minus1Int, BigInteger.ZERO, BigInteger.ONE));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(minus1Int, minus10000Int, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(minus1Dec, minus10000Int, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(minus1Dec, minus10000Dec, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(minus1Dec, minus10000Dec, plus10000Dec));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(minus1Int, minus10000Dec, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(minus1Int, minus10000Int, plus10000Dec));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenRightExclusive(minus1Int, minus10000Dec, plus10000Dec));
-    }
-
-    @Test
-    void testIsBetweenExclusive() {
-        BigInteger minus1Int = BigInteger.valueOf(-1);
-        BigDecimal minus1Dec = BigDecimal.valueOf(-1);
-        BigInteger minus10000Int = BigInteger.valueOf(-10000);
-        BigInteger plus10000Int = BigInteger.valueOf(10000);
-        BigDecimal minus10000Dec = BigDecimal.valueOf(-10000);
-        BigDecimal plus10000Dec = BigDecimal.valueOf(10000);
+    void testIsBetweenExclusive_long() {
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461453366231L, -2147479015L, 292805461453366231L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461453366231L, -2147479015L, 292805461453366231L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461453366231L, 292805461453366231L, 292805461453366231L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461453366231L, 292805461453366231L, 292805461453366232L));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461453366231L, 292805461453366230L, 292805461453366231L));
         Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(292805461453366231L, -2147479015L, 292805461453366232L));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(292805461453366231L, -2147479015L, 292805461453366230L));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-292805461453366231L, -2147479015L, 292805461453366230L));
+    }
+
+    @Test
+    void testIsBetweenExclusive_double() {
+        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(29280546145336.0d, -2147479015.0d, 29280546145336.1d));
+        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(29280546145336.0d, -2147479015.0d, 29280546145337.0d));
+        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-29280546145336.0d, -2147479015.0d, 29280546145336.0d));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0.0d, 0, 1));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0.0d, 0.0d, 1));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(0, -0.0004, 0));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(0, -0.0004, 0.0000f));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0.0d, 0.0d, 1.0d));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0.0d, 0.0d, 1.0f));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0.0d, 0.0f, 1.0f));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0.0d, 0.0f, 1.0d));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(-0.0d, 0.0f, 1));
         Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(0.0d, -0.0f, 1));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(BigInteger.ZERO, BigDecimal.ONE, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(BigInteger.ZERO, BigInteger.ONE, BigDecimal.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(BigInteger.ZERO, BigDecimal.ZERO, BigInteger.ONE));
-        Assertions.assertFalse(ArithmeticUtils.isBetweenExclusive(minus1Int, BigInteger.ZERO, minus1Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(minus1Int, minus10000Int, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(minus1Dec, minus10000Int, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(minus1Dec, minus10000Dec, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(minus1Dec, minus10000Dec, plus10000Dec));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(minus1Int, minus10000Dec, plus10000Int));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(minus1Int, minus10000Int, plus10000Dec));
-        Assertions.assertTrue(ArithmeticUtils.isBetweenExclusive(minus1Int, minus10000Dec, plus10000Dec));
     }
 }
